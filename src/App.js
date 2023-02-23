b5a0595bd9cc2f1a96412e1ed055ede89b997649
arun mudiraj <arunmudiraj18@gmail.com>
2023-02-23 13:33:32 +0530

    error message shown

--- src/App.js
+++ src/App.js
@@ -6,10 +6,20 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
   const fetchMoviesHandler = async ()=>{
-    setIsLoading(true)
     
-    const rawData = await fetch('https://swapi.dev/api/films/')
+
+    
+    setError(null)
+    setIsLoading(true)
+    try{
+     
+      const rawData = await fetch('https://swapi.dev/api/film/') 
+    if (!rawData.ok){
+      setError("Something went wrong!")
+      throw new Error('Something went wrong!')
+    }
     const response = await rawData.json()
     const myMoviesList = response.results.map(item=> {
       return {
@@ -19,7 +29,15 @@ function App() {
       }
     })
     setMovies(myMoviesList)
+    
+    }
+    catch(err){
+      setError(err.message)
+  
+    }
     setIsLoading(false)
+  
+
   }
 
   return (
@@ -31,7 +49,9 @@ function App() {
       <section>
        {!isLoading && movies.length>0 && <MoviesList movies={movies} />}
         {isLoading && <p>Loading...</p>}
-        {!isLoading && movies.length===0 && <p>No movies found</p>}
+        {!isLoading && movies.length===0 && !error && <p>No movies found</p>}
+        {error&& <p>{error}</p>}
+       
         
       </section>
     </React.Fragment>
